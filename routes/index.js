7c09df03f1138997e00e94258186dbfbeb13e9bd
sohamsavalapurkar <soham1999@gmail.com>
2021-11-30 00:40:36 -0600

    item and main page done

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var collection = db.get('sandwiches');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.redirect('/sandwiches');
+	res.render('main');
 });
 
 router.get('/sandwiches', function(req, res) {
@@ -22,6 +22,10 @@ router.get('/sandwiches/:id', function(req, res) {
 		if (err) throw err;
 			res.render('item', {sandwich: sandwich});  
 		});
-})
+});
+
+router.get('/signup', function(req, res) {
+	res.render('signup');
+});
 
 module.exports = router;
