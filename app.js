42540380434e473e49136b17d483213ec62103a3
sohamsavalapurkar <soham1999@gmail.com>
2021-11-29 21:45:14 -0600

    app.js changed

--- app.js
+++ app.js
@@ -3,24 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOveride = require('method-override');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var videosRouter = require('./routes/videos');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOveride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/videos', videosRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
