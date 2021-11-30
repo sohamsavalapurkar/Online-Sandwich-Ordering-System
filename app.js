a74db426f9c99037fa48ea3e20b27f8c8e501476
sohamsavalapurkar <soham1999@gmail.com>
2021-11-30 00:09:41 -0600

    index.ejs page done.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var methodOveride = require('method-override');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var videosRouter = require('./routes/videos');
+
 
 var app = express();
 
@@ -24,24 +24,24 @@ app.use(methodOveride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/videos', videosRouter);
+
 
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var monk = require('monk');
+var db = monk('localhost:27017/sandwiches');
+var collection = db.get('sandwiches');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.redirect('/sandwiches');
 });
+
+router.get('/sandwiches', function(req, res) {
+	collection.find({}, function(err, sandwiches) {
+		if (err) throw err;
+		res.render('index', {results: sandwiches});
+	})
+});
+
+router.get('/sandwiches/:id', function(req, res) {
+	collection.findOne({_id : req.params.id}, function(err, sandwich) {
+		if (err) throw err;
+			res.render('item', {sandwich: sandwich});  
+		});
+})
 
 module.exports = router;
